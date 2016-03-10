7cd30efbf9fcf2b0e252febe4082db60152efd05
2015winter <liuyangguang2011@aliyun.com>
2016-03-10 22:42:49 +0800

    commit cpp-sprite3dTouch-test

---
+++ README.md
@@ -0,0 +1,1 @@
+A template for Cocos2dx 3d model display(girl and tortoise.)
---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+E:\MyWorkSpace\WorkSpaceCocos2dx\MyCocosProjects\cpp-sprite3d-test\proj.android\gen\com\sprite3d\mytest\R.java \
+ : E:\MyWorkSpace\WorkSpaceCocos2dx\MyCocosProjects\cpp-sprite3d-test\proj.android\res\drawable-hdpi\icon.png \
+E:\MyWorkSpace\WorkSpaceCocos2dx\MyCocosProjects\cpp-sprite3d-test\proj.android\res\drawable-ldpi\icon.png \
+E:\MyWorkSpace\WorkSpaceCocos2dx\MyCocosProjects\cpp-sprite3d-test\proj.android\res\drawable-mdpi\icon.png \
+E:\MyWorkSpace\WorkSpaceCocos2dx\MyCocosProjects\cpp-sprite3d-test\proj.android\res\values\strings.xml \
+E:\MyWorkSpace\WorkSpaceCocos2dx\MyCocosProjects\cpp-sprite3d-test\proj.android\bin\AndroidManifest.xml \
